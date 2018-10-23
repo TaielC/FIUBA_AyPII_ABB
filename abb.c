1c03f5b526d7c569c6f5e4e2ec1b42735fd650db
TC <taielcolavecchia@gmial.com>
2018-10-23 19:50:37 -0300

    Creadas estructuras para ABB

--- abb.c
+++ abb.c
@@ -2,3 +2,36 @@
 #include <stdbool.h>
 
 #include "abb.h"
+
+/* ============ TIPOS DE DATOS PARA ABB =========== */
+
+typedef struct abb_nodo{
+	struct abb* izq;
+	struct abb* der;
+	char* clave;
+	void* dato;
+}abb_nodo_t;
+
+typedef struct abb{
+	abb_nodo_t* raiz;
+	abb_comparar_clave_t comparar_clave;
+	abb_destruir_dato_t destruir_dato;
+	int cantidad;
+}abb_t;
+
+/* ------------- FUNCIONES AUXILIARES ------------- */
+
+
+/* ============== PRIMITIVAS DE ABB ============== */
+
+
+
+/* =========== PIMITIVA DEL ITER INTERNO =========== */
+
+
+
+
+/* ========== PIMITIVAS DEL ITER INTERNO ========== */
+
+
+
